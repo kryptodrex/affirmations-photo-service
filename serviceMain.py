f9848478558614bba04e980c09cfac3bcce63db1
Joseph Lunine <jel99@drexel.edu>
2020-11-06 12:35:45 -0500

    added /photos endpoint to retrieve photo data

--- serviceMain.py
+++ serviceMain.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import json
 
 import apiCalls as api
@@ -18,12 +18,28 @@ def get_affirmation():
 
 @app.route(basePath + '/analyze-entities', methods=['POST'])
 def post_text():
-    data = api.languageProcessing(json.loads(request.data))
+    data = api.analyzeTextEntities(json.loads(request.data))
     return data
 
-@app.route(basePath + '/pictures', methods=['GET'])
-    searchText = request.args.get('search')
-    data = api
+@app.route(basePath + '/photos', methods=['GET'])
+def get_photoSearch():
+    queries = {
+        'search': request.args.get('search'),
+        'size': request.args.get('size')
+    }
+    # print(queries)
+    
+    data = api.searchPictures(queries['search'],queries['size'])
+    return data
+
+### Error handling ###
+@app.errorhandler(400)
+def error_400():
+    response = {
+        "message": "Bad request"
+    }
+    return json.dumps(response)
+
 
 if __name__ == '__main_':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
