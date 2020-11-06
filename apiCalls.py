e8997362f6136e8f05b7aba0524eb310c48f5d6a
Joseph Lunine <jel99@drexel.edu>
2020-11-06 12:35:21 -0500

    added searchpictures function

--- apiCalls.py
+++ apiCalls.py
@@ -1,15 +1,21 @@
 import requests
 import json
+import random
 
 import apiEndpoints as endpoint
 import localConfig as config
+
+configs = {
+    'languageApiKey': config.languageApi_key,
+    'unsplashApiKey': config.unsplash_key
+}
 
 
 def affirmations():
     response = requests.get(endpoint.affirmations)
     return response.json()
 
-def languageProcessing(input):
+def analyzeTextEntities(input):
     jsonBody = json.dumps({
         'document': {
             'type': 'PLAIN_TEXT',
@@ -20,14 +26,37 @@ def languageProcessing(input):
 
     # Call Google language API to get entities from text    
     response = requests.post(
-        url=endpoint.languageApi, 
+        url=endpoint.languageApi + '/documents:analyzeEntities', 
         data=jsonBody, 
         params={
-            'key': config.languageApi_key
+            'key': configs['languageApiKey']
         }
     )
 
     return response.json()
 
-def pictures():
-    
+def searchPictures(search, size):
+    response = (requests.get(
+        url=endpoint.unsplash + '/search/photos',
+        params={
+            'query': search,
+            'content_filter': 'high',
+            'per_page': 50
+        },
+        headers={
+            'Authorization': 'Client-ID ' + configs['unsplashApiKey']
+        }
+    )).json()
+
+    choosePhoto = random.choice(response['results'])
+
+    photoData = {
+        'url': choosePhoto['urls'][size],
+        'user': {
+            'name': choosePhoto['user']['name'],
+            'portfolio': choosePhoto['user']['portfolio_url'],
+            'profile': choosePhoto['user']['links']['html']
+        }
+    }
+
+    return photoData
