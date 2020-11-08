f5fc7074253b4c5ccb4a4be957e956646ba2f542
Joseph Lunine <jel99@drexel.edu>
2020-11-07 23:41:23 -0500

    added logs; added download photo endpoint

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def service_status():
 
 @app.route(basePath + '/affirmation', methods=['GET'])
 def get_affirmation():
+    print('log: get affirmation')
     reloadToken = request.args.get('reloadToken')
     data = api.affirmations(reloadToken)
     return data
@@ -23,12 +24,14 @@ def get_affirmation():
 # Analyze the text for its entities
 @app.route(basePath + '/analyze-entities', methods=['POST'])
 def post_text():
+    print('log: post text')
     data = api.analyzeTextEntities(json.loads(request.data))
     return data
 
 # Retrieve a random photo based on search query
 @app.route(basePath + '/photos', methods=['GET'])
 def get_photos():
+    print('log: search photos')
     queries = {
         'search': request.args.get('search'),
         'size': request.args.get('size')
@@ -40,8 +43,16 @@ def get_photos():
 # Retrieve a photo by its ID
 @app.route(basePath + '/photos/<photoId>', methods=['GET'])
 def get_photoById(photoId):
+    print('log: get photo by id')
     size = request.args.get('size')
     data = api.getPhotoById(photoId, size)
+    return data
+
+# Download a photo by its ID
+@app.route(basePath + '/photos/<photoId>/download', methods=['GET'])
+def get_downloadPhoto(photoId):
+    print('log: download photo')
+    data = api.downloadPhoto(photoId)
     return data
 
 ### Error handling ###
